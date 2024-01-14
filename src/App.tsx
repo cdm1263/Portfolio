29d9deafccd0b5576e7796f6dde8cce61046bdde
ChaDongMin <cdm1263@gmail.com>
2024-01-14 19:05:07 +0900

    Feat: Contact 컴포넌트 적용 및 Wrapper 스타일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import About from "./components/about/About";
 import Navbar from "./components/navbar/Navbar";
 import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
   return (
@@ -9,13 +10,13 @@ const App = () => {
       <Navbar />
       <About />
       <Projects />
+      <Contact />
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  max-width: 1200px;
-  margin: auto;
+  position: relative;
 `;
 
 export default App;
