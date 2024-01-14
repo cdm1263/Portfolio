c25240eea94a5a7a6b9c9a6a4b447d4cd1930207
ChaDongMin <cdm1263@gmail.com>
2024-01-14 19:07:46 +0900

    Feat: 네비게이션바 기본 기능 구성

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,59 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { hover } from "../../lib/variants";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Wrapper>
+      <Container>
+        <Title>Dongmin's Portfolio</Title>
+        <ButtonContainer>
+          <Link whileHover={hover.link} href="#about">
+            About
+          </Link>
+          <Link whileHover={hover.link} href="#projects">
+            Projects
+          </Link>
+          <Link whileHover={hover.link} href="#contact">
+            Contact
+          </Link>
+        </ButtonContainer>
+      </Container>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.nav`
+  width: 100vw;
+  height: 100px;
+  position: fixed;
+  // 임시
+  background-color: yellowgreen;
+  opacity: 0.7;
+`;
+
+const Container = styled.div`
+  height: 100%;
+  margin: auto;
+  display: flex;
+  padding: 0 20px;
+  max-width: 1200px;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Link = styled(motion.a)`
+  text-decoration: none;
+  color: inherit;
+`;
 
 export default Navbar;
