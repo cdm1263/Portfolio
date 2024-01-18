8ae7fabeb5471e45bebb338bfaa0897e3b34cddd
ChaDongmin <cdm1263@gmail.com>
2024-01-18 22:56:24 +0900

    Feat: header와 link 객체 생성

--- src/lib/variants.ts
+++ src/lib/variants.ts
@@ -1,7 +1,17 @@
-const variants = { initial: {}, animate: {} };
+const header = {
+  textVariant: {
+    initial: { opacity: 0, y: "-50px" },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1, staggerChildren: 1.3 },
+    },
+  },
+};
 
-const hover = { link: { opacity: 1, transition: { duration: 0.1 } } };
+const link = {
+  hover: { opacity: 1, transition: { duration: 0.1 } },
+  tap: { backgroundColor: "#454545" },
+};
 
-const focus = {};
-
-export { variants, hover, focus };
+export { header, link };
