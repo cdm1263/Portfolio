3cd3ea3bbad3fbc091e0139c3ab660be180b08ba
ChaDongmin <cdm1263@gmail.com>
2024-01-18 21:44:44 +0900

    Feat: Header 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import About from "./components/about/About";
 import Navbar from "./components/navbar/Navbar";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
+import Header from "./components/header/Header";
 
 const App = () => {
   return (
     <Wrapper>
       <Navbar />
+      <Header />
       <About />
       <Projects />
       <Contact />
