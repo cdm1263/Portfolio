bdd317a76a0c4b01480e21a17d181bd0a3bd8aaf
ChaDongmin <cdm1263@gmail.com>
2024-01-18 22:57:35 +0900

    Design: whileTap 애니메이션 추가 및 외형 스타일링

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,20 +1,25 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { hover } from "../../lib/variants";
+import { link } from "../../lib/variants";
 
 const Navbar = () => {
+  const { hover, tap } = link;
+
   return (
     <Wrapper>
       <Container>
         <Title>Dongmin's Portfolio</Title>
         <ButtonContainer>
-          <Link whileHover={hover.link} href="#about">
+          <Link whileHover={hover} whileTap={tap} href="/">
+            Home
+          </Link>
+          <Link whileHover={hover} whileTap={tap} href="#about">
             About
           </Link>
-          <Link whileHover={hover.link} href="#projects">
+          <Link whileHover={hover} whileTap={tap} href="#projects">
             Projects
           </Link>
-          <Link whileHover={hover.link} href="#contact">
+          <Link whileHover={hover} whileTap={tap} href="#contact">
             Contact
           </Link>
         </ButtonContainer>
@@ -24,21 +29,25 @@ const Navbar = () => {
 };
 
 const Wrapper = styled.nav`
+  display: flex;
+  align-items: center;
   width: 100vw;
-  height: 100px;
+  height: 150px;
   position: fixed;
-  // 임시
-  background-color: yellowgreen;
   opacity: 0.7;
 `;
 
 const Container = styled.div`
-  height: 100%;
+  height: 80px;
   margin: auto;
   display: flex;
-  padding: 0 20px;
+  flex: 1;
+  padding: 0 50px;
+  border-radius: 50px;
   max-width: 1200px;
   align-items: center;
+  box-sizing: border-box;
+  background-color: #454545;
   justify-content: space-between;
 `;
 
@@ -48,14 +57,18 @@ const Title = styled.h1`
 
 const ButtonContainer = styled.div`
   display: flex;
-  gap: 20px;
+  gap: 10px;
 `;
 
 const Link = styled(motion.a)`
+  display: block;
+  padding: 15px 20px;
   text-decoration: none;
   color: inherit;
+  border-radius: 8px;
   opacity: 0.7;
   font-weight: 900;
+  box-sizing: border-box;
 `;
 
 export default Navbar;
