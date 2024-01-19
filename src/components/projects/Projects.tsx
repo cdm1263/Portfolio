d536c58a65b7446cdd2dff1822dd76841d52d04d
ChaDongmin <cdm1263@gmail.com>
2024-01-19 18:34:49 +0900

    Feat: ProgressBar 구현

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,12 +1,27 @@
 import styled from "styled-components";
 import generateProjectArray from "../../lib/generateProjectArray";
 import Project from "./Project";
+import { motion, useScroll, useSpring } from "framer-motion";
+import { useRef } from "react";
 
 const Projects = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end end"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
   const projectArray = generateProjectArray();
-  console.log(projectArray);
+
   return (
-    <Wrapper id="projects">
+    <Wrapper id="projects" ref={ref}>
+      <ProgressBar style={{ scaleX, transformOrigin: "0%" }} />
       {projectArray.map((project) => (
         <Project key={project.id} project={project} />
       ))}
@@ -14,6 +29,20 @@ const Projects = () => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled(motion.div)`
+  position: relative;
+`;
+
+const ProgressBar = styled(motion.div)`
+  position: sticky;
+  background-color: #fff;
+  height: 10px;
+  width: 100%;
+  max-width: 1200px;
+  top: 160px;
+  left: 0;
+  right: 0;
+  margin: auto;
+`;
 
 export default Projects;
