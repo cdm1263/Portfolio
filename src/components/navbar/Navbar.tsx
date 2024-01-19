0e5909552c3fd40b81e2f41193bcb126354faa0c
ChaDongmin <cdm1263@gmail.com>
2024-01-19 17:02:05 +0900

    Feat: 애니메이션 추가

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,25 +1,57 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { link } from "../../lib/variants";
+import { link, nav, title } from "../../lib/variants";
 
 const Navbar = () => {
-  const { hover, tap } = link;
+  const { hover, tap, linkVariant } = link;
+  const { titleVariant } = title;
+  const { navVariant } = nav;
 
   return (
     <Wrapper>
-      <Container>
-        <Title>Dongmin's Portfolio</Title>
+      <Container variants={navVariant} initial="initial" animate="animate">
+        <Title variants={titleVariant} initial="initial" animate="animate">
+          Dongmin's Portfolio
+        </Title>
         <ButtonContainer>
-          <Link whileHover={hover} whileTap={tap} href="/">
+          <Link
+            variants={linkVariant}
+            whileHover={hover}
+            initial="initial"
+            animate="animate"
+            whileTap={tap}
+            href="/"
+          >
             Home
           </Link>
-          <Link whileHover={hover} whileTap={tap} href="#about">
+          <Link
+            variants={linkVariant}
+            whileHover={hover}
+            initial="initial"
+            animate="animate"
+            whileTap={tap}
+            href="#about"
+          >
             About
           </Link>
-          <Link whileHover={hover} whileTap={tap} href="#projects">
+          <Link
+            variants={linkVariant}
+            whileHover={hover}
+            initial="initial"
+            animate="animate"
+            whileTap={tap}
+            href="#projects"
+          >
             Projects
           </Link>
-          <Link whileHover={hover} whileTap={tap} href="#contact">
+          <Link
+            variants={linkVariant}
+            whileHover={hover}
+            initial="initial"
+            animate="animate"
+            whileTap={tap}
+            href="#contact"
+          >
             Contact
           </Link>
         </ButtonContainer>
@@ -37,7 +69,7 @@ const Wrapper = styled.nav`
   opacity: 0.7;
 `;
 
-const Container = styled.div`
+const Container = styled(motion.div)`
   height: 80px;
   margin: auto;
   display: flex;
@@ -47,15 +79,15 @@ const Container = styled.div`
   max-width: 1200px;
   align-items: center;
   box-sizing: border-box;
-  background-color: #454545;
+  background-color: ${({ theme }) => theme.bg[300]};
   justify-content: space-between;
 `;
 
-const Title = styled.h1`
+const Title = styled(motion.h1)`
   font-size: 30px;
 `;
 
-const ButtonContainer = styled.div`
+const ButtonContainer = styled(motion.div)`
   display: flex;
   gap: 10px;
 `;
