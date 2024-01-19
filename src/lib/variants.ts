7e14aae55508bd6601a24b644aa987fb29086d5a
ChaDongmin <cdm1263@gmail.com>
2024-01-19 17:01:45 +0900

    Feat: NavBar 애니메이션 variant 추가

--- src/lib/variants.ts
+++ src/lib/variants.ts
@@ -12,6 +12,45 @@ const header = {
 const link = {
   hover: { opacity: 1, transition: { duration: 0.1 } },
   tap: { backgroundColor: "#454545" },
+  linkVariant: {
+    initial: { opacity: 0, y: -10, display: "none" },
+    animate: {
+      opacity: 1,
+      y: 0,
+      display: "block",
+      transition: { opacity: { delay: 1.8 }, y: { duration: 0.5, delay: 1.8 } },
+    },
+  },
 };
 
-export { header, link };
+const title = {
+  titleVariant: {
+    initial: { opacity: 0, y: -10, display: "none" },
+    animate: {
+      opacity: 1,
+      y: 0,
+      display: "block",
+      transition: { opacity: { delay: 1.4 }, y: { duration: 0.5, delay: 1.4 } },
+    },
+  },
+};
+
+const nav = {
+  navVariant: {
+    initial: { flex: 0, y: "-200px", width: "80px", padding: 0, opacity: 0.2 },
+    animate: {
+      flex: 1,
+      opacity: 1,
+      y: 0,
+      width: "100%",
+      padding: "0 50px",
+      transition: {
+        y: { duration: 0.3 },
+        flex: { duration: 1.5, delay: 0.3, ease: "easeInOut" },
+        opacity: { duration: 1.3, ease: "easeInOut" },
+      },
+    },
+  },
+};
+
+export { header, link, title, nav };
