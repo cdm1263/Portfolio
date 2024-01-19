4f7bf3f322391919ff99b268fdf5c645b3081154
ChaDongmin <cdm1263@gmail.com>
2024-01-19 17:48:36 +0900

    Feat: localStorage에 테마 상태를 담아 항상 사용자가 원하는 다크/라이트 테마를 볼 수 있도록 처리

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,25 @@ import Navbar from "./components/navbar/Navbar";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 import Header from "./components/header/Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme.ts";
 import GlobalStyles from "./styles/GlobalStyles.ts";
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(true);
+  const storedDarkMode = localStorage.getItem("isDarkMode");
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(
+    storedDarkMode !== null ? JSON.parse(storedDarkMode) : true
+  );
   const currentTheme = isDarkMode ? theme.darkTheme : theme.lightTheme;
 
   const toggletheme = () => {
     setIsDarkMode((prev) => !prev);
   };
+
+  useEffect(() => {
+    localStorage.setItem("isDarkMode", JSON.stringify(isDarkMode));
+  }, [isDarkMode]);
 
   return (
     <ThemeProvider theme={currentTheme}>
