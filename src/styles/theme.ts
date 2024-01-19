5cd4ceb83b79c1381ecd4398ca960ee65fdd3b63
ChaDongmin <cdm1263@gmail.com>
2024-01-19 16:47:25 +0900

    Feat: theme 생성 및 다크, 라이트 모드를 위한 테마 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,48 @@
+const darkTheme = {
+  primary: {
+    100: "#0085ff",
+    200: "#69b4ff",
+    300: "#e0ffff",
+  },
+  accent: {
+    100: "#006fff",
+    200: "#e1ffff",
+  },
+  text: {
+    100: "#FFFFFF",
+    200: "#9e9e9e",
+  },
+  bg: {
+    100: "#1E1E1E",
+    200: "#2d2d2d",
+    300: "#454545",
+  },
+};
+
+const lightTheme = {
+  primary: {
+    100: "#F0F0F0",
+    200: "#C0C0C0",
+    300: "#808080",
+  },
+  accent: {
+    100: "#FF4500",
+    200: "#FFD700",
+  },
+  text: {
+    100: "#000000",
+    200: "#404040",
+  },
+  bg: {
+    100: "#FFFFFF",
+    200: "#F0F0F0",
+    300: "#C0C0C0",
+  },
+};
+
+const theme = {
+  darkTheme,
+  lightTheme,
+};
+
+export default theme;
