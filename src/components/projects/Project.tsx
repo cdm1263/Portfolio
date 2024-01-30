b7da8019d78151b513e3daf0565aa5ab542d9d0d
ChaDongMin <cdm1263@gmail.com>
2024-01-30 16:34:13 +0900

    Feat: 프로젝트 영역 레이아웃 설정

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -6,20 +6,73 @@ interface ProjectProps {
 }
 
 const Project = ({ project }: ProjectProps) => {
+  const { id, title, term, skills, desc } = project;
+
   return (
     <Wrapper>
-      <Title>{project.title}</Title>
-      {project.skills.map((skill: string) => (
-        <Skill key={skill}>{skill}</Skill>
-      ))}
-      <Description>{project.desc}</Description>
+      <ProgressBarArea />
+      <Title>{title}</Title>
+      <Term>{term}</Term>
+      <SkillsContainer>
+        {skills.map((skill: string) => (
+          <Skill key={skill}>{skill}</Skill>
+        ))}
+      </SkillsContainer>
+      <DescriptionWrapper>
+        {id % 2 ? (
+          <>
+            <ImageContainer></ImageContainer>
+            <Description>{desc}</Description>
+          </>
+        ) : (
+          <>
+            <Description>{desc}</Description>
+            <ImageContainer></ImageContainer>
+          </>
+        )}
+      </DescriptionWrapper>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.section``;
-const Title = styled.h1``;
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+const ProgressBarArea = styled.div`
+  height: 60px;
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+`;
+
+const Term = styled.h2``;
+
+const SkillsContainer = styled.div`
+  display: flex;
+`;
+
 const Skill = styled.h2``;
-const Description = styled.p``;
+
+const DescriptionWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  gap: 30px;
+`;
+
+const ImageContainer = styled.div`
+  flex: 1;
+  background-color: yellow;
+`;
+
+const Description = styled.p`
+  display: flex;
+  flex: 1;
+  font-size: 20px;
+`;
 
 export default Project;
