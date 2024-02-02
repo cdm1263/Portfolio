48673ffb79f2c60cf497cc29e6dc4355457c486d
ChaDongmin <cdm1263@gmail.com>
2024-02-03 06:43:16 +0900

    Docs: 포트폴리오 내용 일부 추가

--- src/lib/generateProjectArray.ts
+++ src/lib/generateProjectArray.ts
@@ -1,25 +1,37 @@
 const generateProjectArray = () => {
   const project1 = {
     id: 1,
-    title: "Dongmin's Portfolio",
-    term: "24.01",
-    skills: ["React", "Typescript", "styled-components"],
-    desc: "설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.",
+    title: "포켓허브 (PokeHub)",
+    term: "2023. 10 ~ 진행중",
+    skills: ["React", "Typescript", "SCSS"],
+    url: "",
+    github: "https://github.com/side-project-pokehub/my-pokemon",
+    desc: "설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다. ",
   };
 
   const project2 = {
     id: 2,
-    title: "PokeHub",
-    term: "23.10 ~ 진행중",
-    skills: ["React", "Typescript", "SCSS"],
-    desc: "설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.",
+    title: "포트폴리오 (Dongmin's Portfolio)",
+    term: "2024. 01",
+    skills: [
+      "React",
+      "Typescript",
+      "styled-components",
+      "Framer Motion",
+      "Netlify",
+    ],
+    url: "",
+    github: "https://github.com/cdm1263/Portfolio",
+    desc: "포트폴리오 웹사이트입니다. Framer Motion 라이브러리를 사용해 애니메이션을 적용해 높은 퀄리티의 사용자 경험을 제공할 수 있었습니다.",
   };
 
   const project3 = {
     id: 3,
-    title: "Woojoo Land",
-    term: "23.06 ~ 23.07",
+    title: "우주 부동산 (Woojoo Land)",
+    term: "2023. 06 ~ 2023. 07",
     skills: ["React", "Typescript", "styled-components"],
+    url: "https://woojoo-land.netlify.app/",
+    github: "https://github.com/cdm1263/woojoo-land",
     desc: "설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.설명을 적는 칸입니다.",
   };
 
