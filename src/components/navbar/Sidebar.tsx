fbedaf3a128e4aa788c7c0d3acbc4f3697f5ed59
ChaDongMin <cdm1263@gmail.com>
2024-02-25 20:37:55 +0900

    Feat: Sidebar의 외부 클릭시 Sidebar가 닫히도록 구현

--- src/components/navbar/Sidebar.tsx
+++ src/components/navbar/Sidebar.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 import Links from "./Links";
 import { sidebar } from "../../lib/variants";
+import { useEffect, useRef } from "react";
 
 interface SidebarProps {
   toggleTheme: () => void;
@@ -13,9 +14,31 @@ interface SidebarProps {
 const Sidebar = ({ toggleTheme, isDarkMode, open, setOpen }: SidebarProps) => {
   const linkProps = { toggleTheme, isDarkMode, setOpen };
   const { wrapperVariant, backgroundVariant } = sidebar;
+  const sidebarRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target as Node)
+      ) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [setOpen]);
 
   return (
-    <Wrapper animate={open ? "open" : "closed"} variants={wrapperVariant}>
+    <Wrapper
+      ref={sidebarRef}
+      animate={open ? "open" : "closed"}
+      variants={wrapperVariant}
+    >
       <LinkBackground variants={backgroundVariant}>
         <Links {...linkProps} />
       </LinkBackground>
